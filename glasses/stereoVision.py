b70465949086da5edd9261f1b6316a7a65255c1f
Kai Webber <94009325+Waikebber@users.noreply.github.com>
2024-09-23 13:48:52 -0400

    Update stereoVision.py

--- glasses/stereoVision.py
+++ glasses/stereoVision.py
@@ -1,1 +1,7 @@
+"""
+This file is for running stereovision in real-time using imagery from the cameras.
 
+Video results should be seen in realtime if an hdmi connection is given to the rasberry pi.
+
+!!!  Please run ./configCameras.py before running this file  !!!
+"""
