e26b2f5b90de1ec50a8e6de8a9de9a0f9618a386
Kai Webber <94009325+Waikebber@users.noreply.github.com>
2024-09-23 13:49:39 -0400

    Update stereoVision.py

--- glasses/stereoVision.py
+++ glasses/stereoVision.py
@@ -4,4 +4,5 @@ This file is for running stereovision in real-time using imagery from the camera
 Video results should be seen in realtime if an hdmi connection is given to the rasberry pi.
 
 !!!  Please run ./configCameras.py before running this file  !!!
+OpenCV's Stereovision Tutorial: https://learnopencv.com/depth-perception-using-stereo-camera-python-c/
 """
