809c3e6fe6de482f1fd1bf730e4d478c7a4f6925
Kai Webber <94009325+Waikebber@users.noreply.github.com>
2024-09-23 21:32:27 -0400

    Update setup_env.sh

--- glasses/setup_env.sh
+++ glasses/setup_env.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Update and install python3-venv if not already installed
-sudo apt update
 sudo apt install -y python3-venv
 
 # Create the virtual environment
-python3 -m venv my_project_env
+python3 -m venv yolo-env
 
 # Activate the virtual environment
-source my_project_env/bin/activate
+source yolo-env/bin/activate
 
 # Install the required packages
 echo "Installing OpenCV (cv2)..."
@@ -33,3 +32,4 @@ print('All packages installed successfully!')
 deactivate
 
 echo "Setup complete. Virtual environment created and packages installed."
+exho "Activate venv with'source yolo-env/bin/activate'"
