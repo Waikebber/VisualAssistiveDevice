7b296730ed75993079a420b05b5e2bd001983313
Kai Webber <94009325+Waikebber@users.noreply.github.com>
2024-09-29 16:33:30 -0400

    Update snapPicture.py

--- glasses/snapPicture.py
+++ glasses/snapPicture.py
@@ -2,14 +2,17 @@ import cv2
 from picamera2 import Picamera2
 import time
 
+image_size = (640, 480)
+result_path = "./pics/"
+
 # Initialize stereo cameras using Picamera2
 print("Initializing cameras...")
 left_cam = Picamera2(0)  # Camera in cam0 port (left camera)
 right_cam = Picamera2(1)  # Camera in cam1 port (right camera)
 
 # Configure both cameras with a lower resolution to reduce resource usage
-left_cam.configure(left_cam.create_preview_configuration(main={"size": (640, 480), "format": "RGB888"}))
-right_cam.configure(right_cam.create_preview_configuration(main={"size": (640, 480), "format": "RGB888"}))
+left_cam.configure(left_cam.create_preview_configuration(main={"size": image_size, "format": "RGB888"}))
+right_cam.configure(right_cam.create_preview_configuration(main={"size":image_size, "format": "RGB888"}))
 
 # Start the cameras
 left_cam.start()
@@ -31,8 +34,8 @@ while True:
         print("Photo capture initiated...")
         # Save the captured frames
         timestamp = time.strftime("%Y%m%d-%H%M%S")
-        left_filename = f"left_{timestamp}.png"
-        right_filename = f"right_{timestamp}.png"
+        left_filename = f"{result_path}/left_{timestamp}.png"
+        right_filename = f"{result_path}/right_{timestamp}.png"
 
         cv2.imwrite(left_filename, left_frame)
         cv2.imwrite(right_filename, right_frame)
