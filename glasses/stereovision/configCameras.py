cc3687b8952b25d42ed4ae927f8381e438601290
Kai Webber <94009325+Waikebber@users.noreply.github.com>
2024-10-14 17:50:08 -0400

    Delete glasses/stereovision/configCameras.py

--- glasses/stereovision/configCameras.py
+++
@@ -1,5 +0,0 @@
-"""
-OpenCV's camera calibration tutorial can be found here: https://learnopencv.com/camera-calibration-using-opencv/
-
-This file is to calibrate the cameras to get refined results for stereovision.
-"""
