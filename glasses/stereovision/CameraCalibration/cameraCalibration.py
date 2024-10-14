ede80dfd2d49cf68d0d9b732b8d9485bbb61acf4
Kai Webber <94009325+Waikebber@users.noreply.github.com>
2024-10-14 17:46:15 -0400

    Update cameraCalibration.py
    Saves calibration to an xml

--- glasses/stereovision/CameraCalibration/cameraCalibration.py
+++ glasses/stereovision/CameraCalibration/cameraCalibration.py
@@ -2,6 +2,8 @@
 """
 The source code of this file is from openCV:
 https://github.com/spmallick/learnopencv/blob/master/CameraCalibration/cameraCalibration.py
+Modifications were inspired from this YouTube Video:
+https://www.youtube.com/watch?v=uKDAVcSaNZA
 """
 import cv2
 import numpy as np
@@ -10,64 +12,101 @@ import glob
 
 # Defining the dimensions of checkerboard
 CHECKERBOARD = (6,9)
+frameSize = (640,480)
+
+# Termination Criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
-# Creating vector to store vectors of 3D points for each checkerboard image
-objpoints = []
-# Creating vector to store vectors of 2D points for each checkerboard image
-imgpoints = [] 
-
 
 # Defining the world coordinates for 3D points
 objp = np.zeros((1, CHECKERBOARD[0]*CHECKERBOARD[1], 3), np.float32)
 objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
-prev_img_shape = None
 
-# Extracting path of individual image stored in a given directory
-images = glob.glob('/images/*.jpg')
-for fname in images:
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# Store object points and image points.
+objpoints = []
+imgpointsL = [] 
+imgpointsR = [] 
+
+# Grab all calibration images.
+imagesLeft = glob.glob('images/stereoLeft/*png')
+imagesRight = glob.glob('images/stereoRight/*png')
+
+# Iterate through all images, left and right
+for imgLeft, imgRight in zip(imagesLeft, imagesRight):
+    imgL = cv2.imread(imgLeft)
+    imgR = cv2.imread(imgRight)
+    grayL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
+    grayR = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
+    
     # Find the chess board corners
     # If desired number of corners are found in the image then ret = true
-    ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH+
-    	cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
+    retL, cornersL = cv2.findChessboardCorners(grayL, CHECKERBOARD, None)
+    retR, cornersR = cv2.findChessboardCorners(grayR, CHECKERBOARD, None)
+
+    ## Can Try This, not sure what it does
+    # retL, cornersL = cv2.findChessboardCorners(grayL, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH+
+    # 	cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
+    # retR, cornersR = cv2.findChessboardCorners(grayR, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH+
+    # 	cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
+
     
     """
     If desired number of corner are detected,
     we refine the pixel coordinates and display 
     them on the images of checker board
     """
-    if ret == True:
+    if retL == True and retR == True:
         objpoints.append(objp)
-        # refining pixel coordinates for given 2d points.
-        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
         
-        imgpoints.append(corners2)
+        # refining pixel coordinates for given 2d points.
+        cornersL = cv2.cornerSubPix(grayL,cornersL,(11,11),(-1,-1),criteria)
+        imgpointsL.append(cornersL)
+
+        cornersR = cv2.cornerSubPix(grayR,cornersR,(11,11),(-1,-1),criteria)
+        imgpointsR.append(cornersR)
 
         # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, CHECKERBOARD, corners2,ret)
+        cv2.drawChessboardCorners(imgL, CHECKERBOARD, cornersL,retL)
+        cv2.drawChessboardCorners(imgR, CHECKERBOARD, cornersR,retR)
     
-    cv2.imshow('img',img)
-    cv2.waitKey(0)
+        cv2.imshow('img left',imgL)
+        cv2.imshow('img right',imgR)
+        cv2.waitKey(1000)
 
-cv2.destroyAllWindows()
+######################### CALIBRATION #######################################
+## Undistortion
+retL, cameraMatrixL, distL, rvecsL, tvecsL = cv2.calibrateCamera(objpoints, imgpointsL, frameSize, None, None)
+heightL, widthL, channelsL = imgL.shape
+newCameraMatrixL, roi_L = cv2.getOptimalNewCameraMatrix(cameraMatrixL, distL, (widthL, heightL), 1, (widthL, heightL))
 
-h,w = img.shape[:2]
+retR, cameraMatrixR, distR, rvecsR, tvecsR = cv2.calibrateCamera(objpoints, imgpointsR, frameSize, None, None)
+heightR, widthR, channelsR = imgR.shape
+newCameraMatrixR, roi_R = cv2.getOptimalNewCameraMatrix(cameraMatrixR, distR, (widthR, heightR), 1, (widthR, heightR))
 
-"""
-Performing camera calibration by 
-passing the value of known 3D points (objpoints)
-and corresponding pixel coordinates of the 
-detected corners (imgpoints)
-"""
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+#############################################################################
+## Stereovision Calibration
+flags = 0
+flags = cv2.CALIB_FIX_INTRINSIC
 
-print("Camera matrix : \n")
-print(mtx)
-print("dist : \n")
-print(dist)
-print("rvecs : \n")
-print(rvecs)
-print("tvecs : \n")
-print(tvecs)
+criteria_stereo = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+retStereo, newCameraMatrixL, distL, newCameraMatricR, distR, rot, trans, essentialMatrix, fundamentalMatrix = cv2.stereoCalibrate(objpoints, imgpointsL, imgpointsR, newCameraMatrixL, distL, newCameraMatrixR, distR, grayL.shape[::-1], criteria_stereo, flags)
+
+## Rectify
+rectifyScale = 1
+rectL, rectR, projMatrixL, projMatrixR, Q, roi_L, roi_R = cv2.stereoRectify(newCameraMatrixL, distL, newCameraMatrixR, distR, grayL.shape[::-1], rot, trans, rectifyScale, (0,0))
+
+
+stereoMapL = cv2.initUndistortRectifyMap(newCameraMatrixL, distL, rectL, projMatrixL, grayL.shape[::-1], cv2.CV_16SC2)
+stereoMapR = cv2.initUndistortRectifyMap(newCameraMatrixR, distR, rectR, projMatrixR, grayR.shape[::-1], cv2.CV_16SC2)
+
+###############################################################################
+## Calibration Saving
+print("Saving stereovision camera calibration!")
+cv_file = cv2.FileStorage('stereoMap.xml', cv2.FILE_STORAGE_WRITE)
+cv_file.write('stereoMapL_x', stereoMapL[0])
+cv_file.write('stereoMapL_y', stereoMapL[1])
+cv_file.write('stereoMapR_x', stereoMapR[0])
+cv_file.write('stereoMapR_y', stereoMapR[1])
+cv_file.release()
+print("Successfully saved stereovision camera calibration!")
+
